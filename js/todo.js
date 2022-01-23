380ce20b917f772391b97860d98172293e12ad37
chusu91 <79313766+chusu91@users.noreply.github.com>
2022-01-23 07:53:45 +0200

    JS:add todolist check function

--- js/todo.js
+++ js/todo.js
@@ -13,22 +13,38 @@ function saveToDos() {
 }
 
 function deleteToDo(event) {
-  const li = event.target.parentElement;
+  const deleteBtn = event.target.parentElement;
+  const li = deleteBtn.parentElement;
   li.remove();
   toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id)); //li.id is String so change to int
   saveToDos(); //beacuse filter function makes NEW array, so have to save the array once again
 }
 
+function checkToDo(event) {
+  const checkBtn = event.target.parentElement;
+  const todo = checkBtn.nextElementSibling;
+  todo.style.textDecoration = "line-through";
+}
+
 function paintToDo(newTodo) {
+  const liItemContainer = document.createElement("div");
+  liItemContainer.setAttribute("class", "ToDoListItemContainer");
   const li = document.createElement("li");
   li.id = newTodo.id; //id of Dom is always STRING
   const span = document.createElement("span");
   span.innerText = newTodo.text;
-  const button = document.createElement("button");
-  button.innerText = "❌";
-  button.addEventListener("click", deleteToDo);
-  li.appendChild(span); // span is the child of li
-  li.appendChild(button);
+  const checkButton = document.createElement("button");
+  checkButton.setAttribute("class", "checkBtn");
+  checkButton.innerHTML = `<i class="fas fa-check-circle"></i>`;
+  checkButton.addEventListener("click", checkToDo);
+  const deleteButton = document.createElement("button");
+  deleteButton.innerHTML = `<i class="fas fa-trash-alt"></i>`;
+  deleteButton.setAttribute("class", "deleteBtn");
+  deleteButton.addEventListener("click", deleteToDo);
+  liItemContainer.appendChild(checkButton);
+  liItemContainer.appendChild(span); // span is the child of li
+  liItemContainer.appendChild(deleteButton);
+  li.appendChild(liItemContainer);
   toDoList.appendChild(li);
 }
 
@@ -47,7 +63,7 @@ function handleToDoSubmit(event) {
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
-
+// button.addEventListener("click", deleteToDo);
 function sayHello() {}
 
 const savedToDos = localStorage.getItem(TODOS_KEY);
